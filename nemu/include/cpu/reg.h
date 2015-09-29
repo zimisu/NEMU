73e42424a9ffced955db9977416cfa6e36ee245d
tracer-ics2015 <tracer@njuics.org>
2015-09-30 01:04:36 +0800

    >  compile NEMU 14307130198 kanchan Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  01:04:36 up 23:22,  3 users,  load average: 0.15, 0.15, 0.12 40e2765d48820632200eef1c9e89e82bbc209f3b

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -19,7 +19,7 @@ typedef struct {
 		uint32_t _32;
 		uint16_t _16;
 		uint8_t _8[2];
-	} gpr[9];//, eax, ecx, edx, ebx, esp, ebp, esi, edi;
+	} gpr[8];//, eax, ecx, edx, ebx, esp, ebp, esi, edi;
 
 	/* Do NOT change the order of the GPRs' definitions. */
 
