57a6f4ced102bf2a3edbedb59b512766fb54ea68
tracer-ics2015 <tracer@njuics.org>
2015-10-21 01:35:42 +0800

    >  compile NEMU 14307130198 kanchan Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  01:35:42 up 18:14,  4 users,  load average: 0.16, 0.10, 0.06 b528c0522cfdc1066ba96402c892a0bb37bc4ef

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -243,6 +243,7 @@ uint32_t eval(int p, int q, bool *success)
 					if (*success == false) return 0;
 					return -val;
 				}
+				printf("%d\n", *success);
 				uint32_t val1 = eval(p, i-1, success);
 				if (*success == false) return 0;
 				printf("this is switch %d %d %d\n", p, i , q);
