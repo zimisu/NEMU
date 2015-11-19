1ad59983de1eb8be3daffe3b29b7e498ec465d7d
tracer-ics2015 <tracer@njuics.org>
2015-11-20 00:28:40 +0800

    >  compile NEMU 14307130198 kanchan Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  00:28:40 up 7 days,  1:50,  6 users,  load average: 0.12, 0.08, 0.06 a451130ec7c581e7e524b80a8072af071407ee68

--- nemu/src/cpu/decode/decode-template.h
+++ nemu/src/cpu/decode/decode-template.h
@@ -33,7 +33,7 @@ make_helper(concat(decode_si_, SUFFIX)) {
 	op_src->simm = ???
 	 */
 	uint64_t tmp = instr_fetch(eip, DATA_BYTE);
-	uint32_t bits = DATA_BYTE >> 3;
+	uint64_t bits = DATA_BYTE >> 3;
 //	uint64_t mask = (1 << (DATA_BYTE >> 3)) - 1;
 	op_src->simm = (tmp << (32 - bits)) >> (32 - bits);
 //	op_src->simm = tmp & mask;
