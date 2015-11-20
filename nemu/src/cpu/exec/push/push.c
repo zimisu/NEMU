67242fad92feca283b241613d22202762a0d81ae
tracer-ics2015 <tracer@njuics.org>
2015-11-20 19:39:14 +0800

    >  compile NEMU 14307130198 kanchan Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  19:39:14 up 7 days,  9:44,  6 users,  load average: 0.16, 0.09, 0.06 63730859c6f16a8dd3d8d0edbaca2f423cbdf9f2

--- nemu/src/cpu/exec/push/push.c
+++ nemu/src/cpu/exec/push/push.c
@@ -19,9 +19,9 @@
 */
 make_helper(push)
 {
-	printf("%x\n",ops_decoded.opcode & 5);
-	printf("%x\n",cpu.gpr[ops_decoded.opcode & 5]._32);
-	swaddr_write(cpu.sp-4, 4, cpu.gpr[ops_decoded.opcode & 5]._32);
+	printf("%x\n",ops_decoded.opcode & 7);
+	printf("%x\n",cpu.gpr[ops_decoded.opcode & 7]._32);
+	swaddr_write(cpu.sp-4, 4, cpu.gpr[ops_decoded.opcode & 7]._32);
 //	MEM_W(cpu.sp-4, REG(op_decoded.opcode & 5));
 	cpu.sp -= 4;
 	print_asm_template2();
