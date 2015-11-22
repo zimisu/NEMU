6cfb46eba1b49698d32472ee3ed5702617901e93
tracer-ics2015 <tracer@njuics.org>
2015-11-22 16:53:01 +0800

    >  compile NEMU 14307130198 kanchan Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  16:53:01 up 8 days,  1:38,  5 users,  load average: 0.16, 0.06, 0.06 19940c275017cdc1a899ca2c074d0ea5e450eaf

--- nemu/src/cpu/exec/arith/cmp.c
+++ nemu/src/cpu/exec/arith/cmp.c
@@ -15,4 +15,4 @@
 #include "cmp-template.h"
 #undef DATA_BYTE
 
-make_helper_v(cmp_si2rm)
+make_helper_v(cmp_rm_imm)
--- nemu/src/cpu/exec/arith/cmp.h
+++ nemu/src/cpu/exec/arith/cmp.h
@@ -7,6 +7,6 @@
 #ifndef __CMP_H__
 #define __CMP_H__
 
-make_helper(cmp_si2rm_v);
+make_helper(cmp_rm_imm_v);
 
 #endif
