890c678916910f636842ff4615054020336d059e
tracer-ics2015 <tracer@njuics.org>
2015-11-23 23:13:01 +0800

    >  compile NEMU 14307130198 kanchan Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  23:13:01 up  7:59,  4 users,  load average: 0.17, 0.10, 0.06 a6f9b72eab38e1c39dc9f68fce60b0d7ede0de

--- nemu/src/cpu/exec/test/test-template.h
+++ nemu/src/cpu/exec/test/test-template.h
@@ -12,9 +12,9 @@ static void do_execute()
 {
 	uint32_t result = op_src->val & op_src->val;
 	if (DATA_BYTE == 2) result &= 0xffff;
-	printf("test_ result= %x\n", result);
+	//printf("test_ result= %x\n", result);
 	cpu.EFLAGS.ZF = (result == 0);
-	printf("zf = %x\n", cpu.EFLAGS.ZF);
+	//printf("zf = %x\n", cpu.EFLAGS.ZF);
 	cpu.EFLAGS.CF = cpu.EFLAGS.OF = 0;
 	cpu.EFLAGS.SF = (result >> 31) & 1;
 	uint32_t tmp = result;
