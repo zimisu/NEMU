977a8a8cb99f1093106b8cbf613d475063d923e5
tracer-ics2015 <tracer@njuics.org>
2015-12-06 00:29:57 +0800

    >  compile NEMU 14307130198 kanchan Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  00:29:57 up 1 day,  5:59,  6 users,  load average: 0.04, 0.13, 0.14 bbfdccacecd6048da42ccef807e2c11ae8116b6

--- nemu/src/cpu/exec/arith/cmp-template.h
+++ nemu/src/cpu/exec/arith/cmp-template.h
@@ -12,24 +12,24 @@ static void do_execute()
 {
 	int bits = DATA_BYTE << 3;
 	uint32_t mask = (1 << bits) - 1;
-	//printf("op_dest = %x, mask = %x\n", op_dest->val, mask);
 	uint32_t a = op_dest->val & mask;
 	uint32_t b = op_src->val;
-	//OPERAND_W(op_dest, ans); 
+	uint32_t ans = a - b;
 	
 	cpu.EFLAGS.CF = (b > a);
 	cpu.EFLAGS.ZF = (a - b == 0);
-	//printf("a = %x, b = %x, a-b = %x, zf = %x\n", a,  b , a-b, cpu.EFLAGS.ZF);
 	cpu.EFLAGS.OF = ((a ^ b ^ (a-b)) >> 31) & 1;
 	cpu.EFLAGS.SF = ((a - b) >> 31) & 1;
-	uint32_t tmp = a - b;
-	tmp = tmp & (tmp << 16);
-	tmp = tmp & (tmp << 8);
-	tmp = tmp & (tmp << 4);
-	tmp = tmp & (tmp << 2);
-	tmp = tmp & (tmp << 1);
-	cpu.EFLAGS.PF = (tmp >> 31) & 1;
+	uint32_t tmp = 1;
+	int i;
+	for (i = 0; i < bits-1; i++)
+	{
+		tmp &= (ans & 1);
+		ans >>= 1;
+	}
+	cpu.EFLAGS.PF = tmp & 1;
 
+	
 	print_asm_template2();
 }
 
