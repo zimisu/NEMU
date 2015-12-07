f721c30dbf469cd1fa65bee35cca4c5b6702e342
tracer-ics2015 <tracer@njuics.org>
2015-12-07 16:51:39 +0800

    >  compile NEMU 14307130198 kanchan Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  16:51:39 up 1 day,  9:34,  7 users,  load average: 0.10, 0.07, 0.05 7a451f54e9b7e2a25939347e1efe6988feb798

--- nemu/src/cpu/exec/stack/leave.c
+++ nemu/src/cpu/exec/stack/leave.c
@@ -7,13 +7,16 @@
 
 #include "cpu/exec/helper.h"
 
-
+#define DATA_BYTE 4
+#include "cpu/exec/template-start.h"
 make_helper(leave)
 {
 	cpu.esp = cpu.ebp;
-	cpu.ebp = swaddr_read(cpu.esp, 4);
+	//cpu.ebp = swaddr_read(cpu.esp, 4);
+	cpu.ebp = MEM_R(cpu.esp);
 	cpu.esp += 4;
 	//print_asm_template1();
 	print_asm("leave");
 	return 1;
 }
+#include "cpu/exec/template-end.h"
