41b726d49a99c8251053e89aaf81a67203baed34
tracer-ics2015 <tracer@njuics.org>
2016-03-04 14:45:39 +0800

    >  compile NEMU 14307130198 kanchan Linux ubuntu 3.19.0-15-generic #15-Ubuntu SMP Thu Apr 16 23:32:37 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  14:45:39 up  1:54,  3 users,  load average: 0.00, 0.02, 0.09 bc31e3788bb2e50cf678c56b8710137ad569dbdd

--- nemu/src/cpu/exec/data-mov/movsx-template.h
+++ nemu/src/cpu/exec/data-mov/movsx-template.h
@@ -4,6 +4,7 @@
 
 
 static void do_execute() {
+    /*
     int sign = (op_src->val >> (DATA_BYTE * 8 - 1)) & 1;
     if (sign)
         OPERAND_W(op_dest, op_src->val & 0xffffffff);
@@ -11,6 +12,7 @@ static void do_execute() {
         OPERAND_W(op_dest, op_src->val);
 
     print_asm_template2();
+    */
 }
 
 make_instr_helper(rm2r)
