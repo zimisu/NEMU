150f29c4405ea4627fa32aafb0b606866ec5f629
tracer-ics2015 <tracer@njuics.org>
2016-03-08 11:09:48 +0800

    >  compile NEMU 14307130198 kanchan Linux ubuntu 3.19.0-25-generic #26~14.04.1-Ubuntu SMP Fri Jul 24 21:16:20 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  11:09:48 up 16:31,  3 users,  load average: 0.05, 0.04, 0.05 a00dbb869a957846f52a0af411a829f44d9e69d

--- nemu/src/cpu/exec/jcc/jge-template.h
+++ nemu/src/cpu/exec/jcc/jge-template.h
@@ -5,12 +5,15 @@
 static void do_execute() {
 	if (cpu.EFLAGS.SF == cpu.EFLAGS.OF)
 	{
+        /*
         int l = DATA_BYTE * 8;
         int mask = 0;
         if (l == 8) mask = 0xff;
         else if (l == 16) mask = 0xffff;
         else mask = 0xffffffff;
-		cpu.eip = ((cpu.eip>>l)<<l)+((cpu.eip + op_src->val) & mask);
+		cpu.eip = ((cpu.eip>>l)<<l)+((cpu.eip + op_src->val) & mask);*/
+        cpu.eip += op_src->val;
+        if (DATA_BYTE == 2) cpu.eip &= 0xffff;
         
 	}
 	print_asm_template1();
