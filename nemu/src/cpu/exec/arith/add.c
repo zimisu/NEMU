aeb57288f1b2ca764228dbcf6861a74adf782954
tracer-ics2015 <tracer@njuics.org>
2016-03-11 14:21:34 +0800

    >  compile NEMU 14307130198 kanchan Linux ubuntu 3.19.0-25-generic #26~14.04.1-Ubuntu SMP Fri Jul 24 21:16:20 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  14:21:34 up 11:43,  2 users,  load average: 0.05, 0.06, 0.05 30bfbf9020fdfd537fad20bcf9137d0ddbaf4cfc

--- nemu/src/cpu/exec/arith/add.c
+++ nemu/src/cpu/exec/arith/add.c
@@ -6,11 +6,11 @@
  ************************************************************************/
 
 #include "cpu/exec/helper.h"
-/*
+
 #define DATA_BYTE 1
 #include "add-template.h"
 #undef DATA_BYTE
-*/
+
 #define DATA_BYTE 2
 #include "add-template.h"
 #undef DATA_BYTE
@@ -19,5 +19,8 @@
 #include "add-template.h"
 #undef DATA_BYTE
 
+make_helper_v(add_i2a)
+make_helper_v(add_i2rm)
+make_helper_v(add_rm2r)
 make_helper_v(add_r2rm)
 make_helper_v(add_si2rm)
