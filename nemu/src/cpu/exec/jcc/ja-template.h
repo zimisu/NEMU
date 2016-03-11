295d6e7a82d727937c093b870efd713db85cdd43
tracer-ics2015 <tracer@njuics.org>
2016-03-11 14:13:47 +0800

    >  compile NEMU 14307130198 kanchan Linux ubuntu 3.19.0-25-generic #26~14.04.1-Ubuntu SMP Fri Jul 24 21:16:20 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  14:13:47 up 11:35,  2 users,  load average: 0.11, 0.07, 0.05 65498acc79bcef2b20d5fc73b28a3c82abb1f4d9

---
+++ nemu/src/cpu/exec/jcc/ja-template.h
@@ -0,0 +1,27 @@
+#include "cpu/exec/template-start.h"
+
+#define instr ja
+
+static void do_execute() {
+    int l = DATA_BYTE << 3;
+    int mask = 0xffffffff;
+    if (DATA_BYTE == 1) mask = 0xff;
+    else if (DATA_BYTE == 2) mask = 0xffff;
+    printf("op_src->val : %x\n", op_src->val);
+	if (cpu.EFLAGS.ZF == 0 &&  cpu.EFLAGS.CF == 0)
+	{
+        //cpu.eip += op_src->val;
+		cpu.eip = ((cpu.eip>>l)<<l)+((cpu.eip + op_src->val) & mask);
+        if (DATA_BYTE == 2)
+            cpu.eip &= 0xffff;
+	}
+	print_asm_template1();
+}
+
+
+
+
+make_instr_helper(i)
+
+
+#include "cpu/exec/template-end.h"
