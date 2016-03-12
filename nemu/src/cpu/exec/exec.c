ee8b46bfebead654271c35b61213208801720835
tracer-ics2015 <tracer@njuics.org>
2016-03-12 19:25:53 +0800

    >  compile NEMU 14307130198 kanchan Linux ubuntu 3.19.0-25-generic #26~14.04.1-Ubuntu SMP Fri Jul 24 21:16:20 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  19:25:53 up  1:10,  2 users,  load average: 0.07, 0.07, 0.10 7a6d1d5fc066cbce93dfb395f46a17c256b9bf6

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -65,12 +65,12 @@ make_group(group2_cl_v,
 /* 0xf6 */
 make_group(group3_b,
 	inv, inv, inv, inv,
-	inv, inv, idiv_rm_b, inv)
+	inv, imul_rm2a_b, idiv_rm_b, inv)
 
 /* 0xf7 */
 make_group(group3_v,
 	inv, inv, inv, inv,
-	inv, inv, idiv_rm_v, inv)
+	inv, imul_rm2a_v, idiv_rm_v, inv)
 
 /* 0xfe */
 make_group(group4,
@@ -120,7 +120,7 @@ helper_fun opcode_table [256] = {
 /* 0x5c */	inv, inv, inv, inv,
 /* 0x60 */	inv, inv, inv, inv,
 /* 0x64 */	inv, inv, data_size, inv,
-/* 0x68 */	inv, inv, inv, inv,
+/* 0x68 */	inv, imul_i_rm2r_v, inv, imul_si_rm2r_v,
 /* 0x6c */	inv, inv, inv, inv,
 /* 0x70 */	jo_si_b, jno_si_b, jb_si_b, jae_si_b,
 /* 0x74 */	je_si_b, jne_si_b, jbe_si_b, ja_si_b,
@@ -204,7 +204,7 @@ helper_fun _2byte_opcode_table [256] = {
 /* 0xa0 */	inv, inv, inv, inv,
 /* 0xa4 */	inv, inv, inv, inv,
 /* 0xa8 */	inv, inv, inv, inv,
-/* 0xac */	inv, inv, inv, inv,
+/* 0xac */	inv, inv, inv, imul_rm2r_v,
 /* 0xb0 */	inv, inv, inv, inv,
 /* 0xb4 */	inv, inv, inv, inv,
 /* 0xb8 */	inv, inv, inv, inv,
