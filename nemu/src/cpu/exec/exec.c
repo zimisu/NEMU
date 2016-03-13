9738565bd314c6363fad2c5fc71c941c0f0ee48b
tracer-ics2015 <tracer@njuics.org>
2016-03-13 15:12:39 +0800

    >  compile NEMU 14307130198 kanchan Linux ubuntu 3.19.0-25-generic #26~14.04.1-Ubuntu SMP Fri Jul 24 21:16:20 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  15:12:39 up  2:13,  2 users,  load average: 0.12, 0.08, 0.12 d92bace4b2215d4d8970b0eb5b8df67de713a3bb

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -64,12 +64,12 @@ make_group(group2_cl_v,
 
 /* 0xf6 */
 make_group(group3_b,
-	test_r2rm_b, inv, inv, inv,
+	test_r2rm_b, inv, not_rm_b, inv,
 	inv, imul_rm2a_b, inv, idiv_rm_b)
 
 /* 0xf7 */
 make_group(group3_v,
-	test_r2rm_v, inv, inv, inv,
+	test_r2rm_v, inv, not_rm_v, inv,
 	inv, imul_rm2a_v, inv, idiv_rm_v)
 
 /* 0xfe */
