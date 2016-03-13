4a301d1865440999219b2e768e659ee8099190d7
tracer-ics2015 <tracer@njuics.org>
2016-03-13 19:03:34 +0800

    >  compile NEMU 14307130198 kanchan Linux ubuntu 3.19.0-25-generic #26~14.04.1-Ubuntu SMP Fri Jul 24 21:16:20 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  19:03:34 up  6:04,  2 users,  load average: 0.17, 0.08, 0.09 32a73bc876b048d4f1c1f8a272236304fef7c58

---
+++ testcase/src/my_fib.c
@@ -0,0 +1,25 @@
+#include "trap.h"
+
+
+int fib[] = {1, 1, 2, 3, 5, 8, 13, 21};
+
+#define len sizeof(fib) / sizeof(int)
+
+int cal_fib(int a)
+{
+    if (a == 0 || a == 1) return 1;
+    return cal_fib(a-1) + cal_fib(a-2);
+}
+
+int main()
+{
+    int i;
+    for (i = 0; i < len; i ++)
+        nemu_assert(cal_fib(i) == fib[i]);
+
+    nemu_assert(i == len);
+
+    HIT_GOOD_TRAP;
+
+    return 0;
+}
