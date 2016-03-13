c41746a4f22814ae26c27c24f88ccf469cbfdd7f
tracer-ics2015 <tracer@njuics.org>
2016-03-13 12:39:30 +0800

    >  compile NEMU 14307130198 kanchan Linux ubuntu 3.19.0-25-generic #26~14.04.1-Ubuntu SMP Fri Jul 24 21:16:20 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  12:39:30 up 18 min,  2 users,  load average: 0.03, 0.06, 0.10 dbf912a2f1dc4b1b53929b7c7c0b77236d696812

--- nemu/src/cpu/exec/jmp/jmp_indrct-template.h
+++ nemu/src/cpu/exec/jmp/jmp_indrct-template.h
@@ -6,8 +6,8 @@ static void do_execute() {
 	cpu.eip = op_src->val;
     if (DATA_BYTE == 2) cpu.eip &= 0xffff;
     if (op_dest->type == OP_TYPE_REG)
-        print_asm("jmpsdfa" str(SUFFIX) " %%%s", op_dest->str);
-    else print_asm("jmp" str(SUFFIX) " *0x%x", cpu.eip + 1 + DATA_BYTE);
+        print_asm("jmp" str(SUFFIX) " %%%s", op_dest->str);
+    else print_asm("jmpasdf" str(SUFFIX) " *0x%x", cpu.eip + 1 + DATA_BYTE);
 }
 
 make_instr_helper(rm)
