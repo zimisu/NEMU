2e91220e1ba482b30323eaa4ff8444f6139ffa65
tracer-ics2015 <tracer@njuics.org>
2016-03-13 23:25:02 +0800

    >  run 14307130198 kanchan Linux ubuntu 3.19.0-25-generic #26~14.04.1-Ubuntu SMP Fri Jul 24 21:16:20 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  23:25:02 up 10:22,  2 users,  load average: 0.04, 0.08, 0.07 b9a92b1bc569e9d246b1d78f6cc850d535333e

--- testcase/src/my_switch.c
+++ testcase/src/my_switch.c
@@ -4,19 +4,18 @@
 int f(int i)
 {
     switch (i){
-        case 1: case 2: return 1;
-        //case 2: case 3: return 1; 
-        //case 4: case 5:return 2;
+        case 2: case 3: return 1; 
+        case 4: case 5:return 2;
         default: return 0;
     }
 }
 
 int main()
 {
-    //int i = 0;
+    int i = 0;
     nemu_assert(f(-1) == 0);
-    //for (i = 1; i < 6; i++)
-        //nemu_assert(f(i) == i / 2);
+    for (i = 1; i < 6; i++)
+        nemu_assert(f(i) == i / 2);
     HIT_GOOD_TRAP;
     //printf("%d %d\n", f(0), f(-1));
     return 0;
