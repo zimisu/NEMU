f79e7711163139981babbea6e10e36edd9c7f23c
tracer-ics2015 <tracer@njuics.org>
2016-03-13 19:47:22 +0800

    >  compile NEMU 14307130198 kanchan Linux ubuntu 3.19.0-25-generic #26~14.04.1-Ubuntu SMP Fri Jul 24 21:16:20 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  19:47:22 up  6:44,  2 users,  load average: 0.11, 0.06, 0.10 48da24bbdeba82ba9685be54dc3d6ba3610d0784

--- nemu/src/cpu/exec/call/call-template.h
+++ nemu/src/cpu/exec/call/call-template.h
@@ -7,7 +7,7 @@
 #include "cpu/exec/template-start.h"
 
 #define instr call
-
+/*
 static void do_execute()
 {
 	cpu.esp -= 4;
@@ -21,7 +21,21 @@ static void do_execute()
 
 	print_asm("call 0x%x", cpu.eip + get_instr_len());
 	//print_asm_template1();
+}*/
+
+static void do_execute() {
+	int len = get_instr_len();
+	cpu.esp -= 4;
+	swaddr_write(cpu.esp, 4, cpu.eip + len);
+	if(op_src->type == OP_TYPE_IMM) {
+		cpu.eip += op_src->val;
+		snprintf(op_src->str, OP_STR_SIZE, "$0x%x", cpu.eip + len + 1);
+	}else {
+		cpu.eip = op_src->val - len - 1;
+	}
+	print_asm_template1();
 }
+
 
 make_instr_helper(si);
 make_instr_helper(rm);
