1b44dbc09c4a11e52f9895a7ce120cd45e7975eb
tracer-ics2015 <tracer@njuics.org>
2016-03-13 23:28:11 +0800

    >  compile NEMU 14307130198 kanchan Linux ubuntu 3.19.0-25-generic #26~14.04.1-Ubuntu SMP Fri Jul 24 21:16:20 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  23:28:11 up 10:25,  2 users,  load average: 0.05, 0.12, 0.10 e9b0e89b81189548a5bc6821de99cbb6eeff6bd8

--- nemu/src/cpu/exec/jmp/jmp-template.h
+++ nemu/src/cpu/exec/jmp/jmp-template.h
@@ -14,7 +14,7 @@ static void do_execute() {
 		cpu.eip += op_src->val;
 		//snprintf(op_src->str, OP_STR_SIZE, "$0x%x", cpu.eip + len + 1);
 	} else {
-		cpu.eip = op_src->val - len - 1;
+		cpu.eip = op_src->val - len;
 	}
 	print_asm("jmp" str(SUFFIX) " $%x\n", cpu.eip + len);
 }
