0015243386b1f7b60214e9b3692bbf59f5c5eba3
tracer-ics2015 <tracer@njuics.org>
2016-03-13 22:37:14 +0800

    >  run 14307130198 kanchan Linux ubuntu 3.19.0-25-generic #26~14.04.1-Ubuntu SMP Fri Jul 24 21:16:20 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  22:37:14 up  9:34,  2 users,  load average: 0.10, 0.11, 0.07 dbbff603ae4cf1845c49cac222561f95b076b9d7

--- testcase/src/my_switch.c
+++ testcase/src/my_switch.c
@@ -3,8 +3,8 @@
 int f(int i)
 {
     switch (i){
-        case 1: case 2: return 1; 
-        case 3: case 4:return 2;
+        case 2: case 3: return 1; 
+        case 4: case 5:return 2;
         default: return 0;
     }
 }
