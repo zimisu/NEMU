21d3b980dad10fe4a634cbcffe9037ea8fec760b
tracer-ics2015 <tracer@njuics.org>
2016-03-13 20:11:42 +0800

    >  compile NEMU 14307130198 kanchan Linux ubuntu 3.19.0-25-generic #26~14.04.1-Ubuntu SMP Fri Jul 24 21:16:20 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  20:11:42 up  7:08,  2 users,  load average: 0.11, 0.12, 0.13 f9a08eda2aed4d3dc683cd3de1c544225b62c5f8

--- nemu/src/cpu/exec/call/call-template.h
+++ nemu/src/cpu/exec/call/call-template.h
@@ -18,26 +18,11 @@ static void do_execute()
 		cpu.eip += op_src->val;
 	else
 		cpu.eip = op_src->val - get_instr_len();
-	//if (DATA_BYTE == 2) cpu.eip &= 0xffff;
+	if (DATA_BYTE == 2) cpu.eip &= 0xffff;
 
-	print_asm("call 0x%x", cpu.eip + get_instr_len());
+	print_asm("call $0x%x", cpu.eip + get_instr_len());
 	//print_asm_template1();
 }
-/*
-static void do_execute() {
-	int len = get_instr_len();
-	cpu.esp -= 4;
-	swaddr_write(cpu.esp, 4, cpu.eip + len);
-	if(op_src->type == OP_TYPE_IMM) {
-		cpu.eip += op_src->val;
-		snprintf(op_src->str, OP_STR_SIZE, "$0x%x", cpu.eip + len + 1);
-	}else {
-		cpu.eip = op_src->val - len - 1;
-	}
-	print_asm_template1();
-}
-*/
-
 make_instr_helper(si);
 make_instr_helper(rm);
 
