387d4db6be5a0e24fa8267e0ca82cbbf835fa1b8
tracer-ics2015 <tracer@njuics.org>
2016-03-13 18:30:54 +0800

    >  compile NEMU 14307130198 kanchan Linux ubuntu 3.19.0-25-generic #26~14.04.1-Ubuntu SMP Fri Jul 24 21:16:20 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  18:30:54 up  5:31,  2 users,  load average: 0.28, 0.20, 0.23 1c79360a3d69ad44f3e2b7ddaded7bd822ad031f

--- nemu/src/cpu/exec/stack/pop-template.h
+++ nemu/src/cpu/exec/stack/pop-template.h
@@ -5,17 +5,17 @@ static void do_execute()
 {
     int val = cpu.esp;
     cpu.esp += DATA_BYTE;
-	int opcode = instr_fetch(cpu.eip, 1);
-	if (opcode == 0x8f)
-	{
+	//int opcode = instr_fetch(cpu.eip, 1);
+	//if (opcode == 0x8f)
+	//{
     	OPERAND_W(op_dest, val);
     	print_asm_template1();
-	} else 
+	/*} else 
 	if (opcode >= 0x58 && opcode < 0x58 + 8)
 	{
 		reg_l(opcode - 0x58) = val;
 		print_asm("pop" str(SUFFIX) " %%%s", REG_NAME(opcode - 0x58));
-	}
+	}*/
 }
 
 make_instr_helper(rm)
