6c9690a5d85d5c8a70bdaf0361dd4db345e7e19f
tracer-ics2015 <tracer@njuics.org>
2016-03-13 23:00:28 +0800

    >  run 14307130198 kanchan Linux ubuntu 3.19.0-25-generic #26~14.04.1-Ubuntu SMP Fri Jul 24 21:16:20 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  23:00:28 up  9:57,  2 users,  load average: 0.22, 0.10, 0.06 768def92947cb9d488b9c5ee5e52f9c2c0c70c6

--- testcase/src/my_switch.c
+++ testcase/src/my_switch.c
@@ -1,5 +1,5 @@
-//#include "trap.h"
-#include <stdio.h>
+#include "trap.h"
+//#include <stdio.h>
 
 int f(int i)
 {
@@ -14,10 +14,10 @@ int f(int i)
 int main()
 {
     //int i = 0;
-    //nemu_assert(f(-1) == 0);
-        //for (i = 1; i < 6; i++)
+    nemu_assert(f(-1) == 0);
+    //for (i = 1; i < 6; i++)
         //nemu_assert(f(i) == i / 2);
-    //HIT_GOOD_TRAP;
-    printf("%d %d\n", f(0), f(-1));
+    HIT_GOOD_TRAP;
+    //printf("%d %d\n", f(0), f(-1));
     return 0;
 }
