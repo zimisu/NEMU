8c9bbf3c79ccbbf5afc56b8ad8746bb3d970dbce
tracer-ics2015 <tracer@njuics.org>
2016-03-14 16:30:34 +0800

    >  run 14307130198 kanchan Linux ubuntu 3.19.0-25-generic #26~14.04.1-Ubuntu SMP Fri Jul 24 21:16:20 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  16:30:34 up 16:30,  2 users,  load average: 0.14, 0.14, 0.11 69dbb542fb73cb6e5c477af5f6e360f6ab6c29bd

--- lib-common/FLOAT.c
+++ lib-common/FLOAT.c
@@ -31,11 +31,11 @@ FLOAT F_div_F(FLOAT a, FLOAT b) {
 }
 
 FLOAT f2F(float a) {	
-	int i, uf, m, e, s, ans;
-	uf = *(int*)&a;
-	m = uf & ((1 << 23) - 1);
-	e = ((uf >> 23) & ((1 << 8) - 1)) - 127;
-	s = uf >> 31;
+	int i, m, e, s, ans;
+	uint32_t f_uint = *(uint32*)&a;
+	m = f_uint & 0x7fffff;
+	e = ((f_uint >> 23) & ((1 << 8) - 1)) - 127;
+	s = f_uint >> 31;
 	ans = 1;
 	for(i = 1; i <= e + 16; ++ i) {
 		ans = (ans << 1) + ((m & (1 << 22)) >> 22);
@@ -43,7 +43,7 @@ FLOAT f2F(float a) {
 		m = m << 1;
 	}
 	if (s != 0) ans = (~ans) + 1;
-	return (FLOAT)(ans);
+	return (ans);
 }
 
 FLOAT Fabs(FLOAT a) {
