2d2e515e7a2eaf1d4683ed8af0ff8554aa7797ae
tracer-ics2015 <tracer@njuics.org>
2016-03-14 21:25:07 +0800

    >  compile NEMU 14307130198 kanchan Linux ubuntu 3.19.0-25-generic #26~14.04.1-Ubuntu SMP Fri Jul 24 21:16:20 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  21:25:07 up 20:35,  3 users,  load average: 0.16, 0.13, 0.13 aedf67a0f40cb7c3b295d01f3a24b05aa28b834

--- nemu/src/cpu/exec/data-mov/movzx-template.h
+++ nemu/src/cpu/exec/data-mov/movzx-template.h
@@ -1,20 +1,24 @@
 #include "cpu/exec/template-start.h"
 
-#define instr movzx
-
-
+#define instr movzb
 static void do_execute() {
-    uint32_t mask = 0xffffffff;
-    if (op_src->size == 1)
-       mask = 0xff;
-    else if (op_src->size == 2)
-        mask = 0xffff;
+    uint32_t mask = 0xff;
     OPERAND_W(op_dest, op_src->val & mask);
-    
     print_asm_template2();
 }
 
+make_instr_helper(rm2r)
+#undef instr
+
+/*-----------------------------------------------*/
+#define instr movzw
+static void do_execute() {
+    uint32_t mask = 0xffff;
+    OPERAND_W(op_dest, op_src->val & mask);
+    print_asm_template2();
+}
 
 make_instr_helper(rm2r)
+#undef instr
 
 #include "cpu/exec/template-end.h"
--- nemu/src/cpu/exec/data-mov/movzx.h
+++ nemu/src/cpu/exec/data-mov/movzx.h
@@ -1,7 +1,8 @@
 #ifndef __MOVZX_H__
 #define __MOVZX_H__
 
-make_helper(movzx_rm2r_v);
+make_helper(movzb_rm2r_l);
+make_helper(movzw_rm2r_l);
 
 
 
