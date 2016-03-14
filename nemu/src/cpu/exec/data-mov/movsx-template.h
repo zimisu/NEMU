4ac32070e95da781518ca1046ed0515f81434a51
tracer-ics2015 <tracer@njuics.org>
2016-03-14 21:14:37 +0800

    >  compile NEMU 14307130198 kanchan Linux ubuntu 3.19.0-25-generic #26~14.04.1-Ubuntu SMP Fri Jul 24 21:16:20 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  21:14:37 up 20:25,  3 users,  load average: 0.06, 0.10, 0.13 8fca44d714535f59eba552ad4d49f294e5a65fd

--- nemu/src/cpu/exec/data-mov/movsx-template.h
+++ nemu/src/cpu/exec/data-mov/movsx-template.h
@@ -1,27 +1,35 @@
 #include "cpu/exec/template-start.h"
 
-#define instr movsx
-
-
+#define instr movsb
 static void do_execute() {
-	
-	uint32_t sign = 1 & (op_src->val >> ((op_src->size << 3) - 1));
-	//printf("%d %d %d\n", op_src->type, op_dest->type);		
-	printf("%d %d\n", (int)op_src->size, (int)op_dest->size);		
-//printf("%d %d   %d %d\n", op_src->type, op_src->size, op_dest->type, op_dest->size);
-    //uint32_t sign = MSB(op_src->val);
+	uint32_t sign = op_src->val & 0x80;
     uint32_t mask = 0;
+	if (sign) mask = 0xffffff00;
+/*
     if (op_src->size == 1 && op_dest->size == 2 && sign)
       	mask = 0xff00;
     else if (op_src->size == 1 && op_dest->size == 4 && sign)
         mask = 0xffffff00;
     else if (op_src->size == 2 && op_dest->size == 4 && sign)
-        mask = 0xffff0000;
+        mask = 0xffff0000;*/
     OPERAND_W(op_dest, op_src->val | mask);
-    
     print_asm_template2();
 }
 
 make_instr_helper(rm2r)
+#undef instr
+
+/*-----------------------------------------------*/
+#define instr movsw
+static void do_execute() {
+	uint32_t sign = op_src->val & 0x8000;
+    uint32_t mask = 0;
+	if (sign) mask = 0xffff0000;
+    OPERAND_W(op_dest, op_src->val | mask);
+    print_asm_template2();
+}
+
+make_instr_helper(rm2r)
+#undef instr
 
 #include "cpu/exec/template-end.h"
--- nemu/src/cpu/exec/data-mov/movsx.c
+++ nemu/src/cpu/exec/data-mov/movsx.c
@@ -1,14 +1,10 @@
 #include "cpu/exec/helper.h"
 
-#define DATA_BYTE 2
-#include "movsx-template.h"
-#undef DATA_BYTE
 
 #define DATA_BYTE 4
 #include "movsx-template.h"
 #undef  DATA_BYTE
 
 
-make_helper_v(movsx_rm2r)
 
 
