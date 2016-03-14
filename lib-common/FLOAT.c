76c96d62256ab0622bd974563ce3cb796ff1e853
tracer-ics2015 <tracer@njuics.org>
2016-03-14 15:15:38 +0800

    >  run 14307130198 kanchan Linux ubuntu 3.19.0-25-generic #26~14.04.1-Ubuntu SMP Fri Jul 24 21:16:20 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux  15:15:38 up 15:57,  2 users,  load average: 0.06, 0.09, 0.06 d94c827e2b9b5c262df384bca265e7c02fa4b9

--- lib-common/FLOAT.c
+++ lib-common/FLOAT.c
@@ -1,23 +1,44 @@
 #include "FLOAT.h"
 
+typedef long long int64_t;
+
 FLOAT F_mul_F(FLOAT a, FLOAT b) {
-	nemu_assert(0);
-	return 0;
+	int64_t res = (int64_t)a * (int64_t)b;	
+	//nemu_assert(0);
+	return res >> 16;
 }
 
 FLOAT F_div_F(FLOAT a, FLOAT b) {
-	nemu_assert(0);
-	return 0;
+	unsigned int a00 = a << 16;
+	unsigned int a01 = a >> 16;
+	unsigned int a10 = a >> 31;
+	unsigned int a11 = a >> 31;
+	int ans = 0, i;
+	for(i = 0; i < 64; ++i)
+	{
+		a11 = (a11 << 1) + (a10 >> 31);
+		a10 = (a10 << 1) + (a01 >> 31);
+		a01 = (a01 << 1) + (a00 >> 31);
+		a00 = a00 << 1;
+		ans = ans << 1;
+		if(a11 > 0 || a10 >= b) {
+			if(a10 < b) a11 --;
+			a10 -= b;
+			ans++; 
+		}
+	}
+	return ans;
 }
 
 FLOAT f2F(float a) {
-	nemu_assert(0);
-	return 0;
+	//nemu_assert(0);
+	return (int)(a * (1 << 16));
 }
 
 FLOAT Fabs(FLOAT a) {
-	nemu_assert(0);
-	return 0;
+	//nemu_assert(0);
+	if (a < 0) return -a;
+	return a;
 }
 
 FLOAT sqrt(FLOAT x) {
--- lib-common/FLOAT.h
+++ lib-common/FLOAT.h
@@ -6,23 +6,24 @@
 typedef int FLOAT;
 
 static inline int F2int(FLOAT a) {
-	nemu_assert(0);
-	return 0;
+	//nemu_assert(0);
+	return a >> 16;
 }
 
 static inline FLOAT int2F(int a) {
-	nemu_assert(0);
-	return 0;
+	//nemu_assert(0);
+	return a << 16;
 }
 
 static inline FLOAT F_mul_int(FLOAT a, int b) {
-	nemu_assert(0);
-	return 0;
+	//int64_t res = a * b;
+	return a * b;
 }
 
 static inline FLOAT F_div_int(FLOAT a, int b) {
-	nemu_assert(0);
-	return 0;
+	//nemu_assert(0);
+	//int64_t res = a / b;
+	return a / b;
 }
 
 FLOAT f2F(float);
