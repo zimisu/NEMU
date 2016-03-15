5fd8438f102ae02798538870183dab850700be0b
tracer-ics2015 <tracer@njuics.org>
2016-03-15 16:50:19 +0800

    >  compile kernel 14307130198 kanchan Linux ubuntu 3.19.0-56-generic #62~14.04.1-Ubuntu SMP Fri Mar 11 11:03:15 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  16:50:19 up  7:17,  2 users,  load average: 0.12, 0.15, 0.14 31ecaad8da7ec6966048f09baf033487312856a7

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -122,19 +122,13 @@ uint32_t loader() {
 	/* Load each program segment */
 	//panic("please implement me");
 	int i;
+	ph = (void*)buf + elf->e_phoff;	
 	for(i = 0; i < elf->e_phnum; i++) {
-		ph = (void*)elf->e_phoff + i * elf->e_phentsize;	
 		/* Scan the program header table, load each segment into memory */
 		if(ph->p_type == PT_LOAD) {
 
-			/* TODO: read the content of the segment from the ELF file 
-			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
-			 */
 			ramdisk_read((uint8_t*)(ph->p_vaddr), ph->p_offset, ph->p_filesz);
 			 
-			/* TODO: zero the memory region 
-			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
-			 */
 			memset((void*)ph->p_vaddr + ph->p_filesz, ph->p_memsz - ph->p_filesz, 0);
 
 
@@ -145,6 +139,7 @@ uint32_t loader() {
 			if(brk < new_brk) { brk = new_brk; }
 #endif
 		}
+		ph++;
 	}
 
 	//volatile uint32_t entry = elf->e_entry;
