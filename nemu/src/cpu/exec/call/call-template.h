6f8fc03fc2df467028d154e0d7240bc16b33a00c
tracer-ics2015 <tracer@njuics.org>
2016-03-16 02:30:44 +0800

    >  compile NEMU 14307130198 kanchan Linux ubuntu 3.19.0-56-generic #62~14.04.1-Ubuntu SMP Fri Mar 11 11:03:15 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  02:30:44 up 16:21,  3 users,  load average: 0.01, 0.07, 0.12 2f09c29e9165fddab508821c93aefbd4df40b5c1

--- nemu/src/cpu/exec/call/call-template.h
+++ nemu/src/cpu/exec/call/call-template.h
@@ -12,16 +12,19 @@
 static void do_execute()
 {
 	cpu.esp -= 4;
-	swaddr_write(cpu.esp, 4, cpu.eip + get_instr_len());
+	//swaddr_write(cpu.esp, 4, cpu.eip + get_instr_len());
+	swaddr_write(cpu.esp, 4, cpu.eip + DATA_BYTE + 1);
 
 	if (op_src->type == OP_TYPE_IMM)
 		cpu.eip += op_src->val;
 	else
-		cpu.eip = op_src->val - get_instr_len();
+		//cpu.eip = op_src->val - get_instr_len();
+		cpu.eip = op_src->val - 2;
 	//printf("%d\n", cpu.eip);
 	if (DATA_BYTE == 2) 
 		cpu.eip &= 0xffff;
-	print_asm("call $0x%x", cpu.eip + get_instr_len());
+	//print_asm("call $0x%x", cpu.eip + get_instr_len());
+	print_asm("call $0x%x", cpu.eip + 2);
 	
 	//print_asm_template1();
 }
