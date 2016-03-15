d46c69f07092a46ec58bb81e52a64915a40124e0
tracer-ics2015 <tracer@njuics.org>
2016-03-15 23:43:34 +0800

    >  compile NEMU 14307130198 kanchan Linux ubuntu 3.19.0-56-generic #62~14.04.1-Ubuntu SMP Fri Mar 11 11:03:15 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  23:43:34 up 13:34,  2 users,  load average: 0.15, 0.18, 0.10 3a4bfa0a5b6b95201c0d1ccb3b6135496ea89a3

--- nemu/src/cpu/exec/data-mov/movsx-template.h
+++ nemu/src/cpu/exec/data-mov/movsx-template.h
@@ -5,13 +5,7 @@ static void do_execute() {
 	uint32_t sign = op_src->val & 0x80;
     uint32_t mask = 0;
 	if (sign) mask = 0xffffff00;
-/*
-    if (op_src->size == 1 && op_dest->size == 2 && sign)
-      	mask = 0xff00;
-    else if (op_src->size == 1 && op_dest->size == 4 && sign)
-        mask = 0xffffff00;
-    else if (op_src->size == 2 && op_dest->size == 4 && sign)
-        mask = 0xffff0000;*/
+	
     OPERAND_W(op_dest, op_src->val | mask);
     print_asm_template2();
 }
