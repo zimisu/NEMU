0d981e359ad2ee43650abd558498f438f7927f4e
tracer-ics2015 <tracer@njuics.org>
2016-03-15 22:08:28 +0800

    >  compile NEMU 14307130198 kanchan Linux ubuntu 3.19.0-56-generic #62~14.04.1-Ubuntu SMP Fri Mar 11 11:03:15 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  22:08:28 up 11:59,  2 users,  load average: 0.00, 0.03, 0.05 7dc8477655b71dcb92f268b1bd09df435c6ba0e

--- nemu/src/cpu/exec/arith/dec-template.h
+++ nemu/src/cpu/exec/arith/dec-template.h
@@ -6,8 +6,11 @@ static void do_execute () {
 	DATA_TYPE result = op_src->val - 1;
 	OPERAND_W(op_src, result);
 
-	/* TODO: Update EFLAGS. */
-	panic("please implement me");
+	if(MSB(op_src->val) == 0 && MSB(result) != MSB(op_src->val))
+		cpu.EFLAGS.OF = 1; else cpu.EFLAGS.OF = 0;
+	cpu.EFLAGS.ZF = result == 0;
+	cpu.EFLAGS.SF = MSB(result);
+	cpu.EFLAGS.PF = get_pf(result);
 
 	print_asm_template1();
 }
