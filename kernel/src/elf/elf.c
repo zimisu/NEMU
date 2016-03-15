70a5bcf53ad78f7d1741b1384ab1435ad22db342
tracer-ics2015 <tracer@njuics.org>
2016-03-15 16:54:20 +0800

    >  compile kernel 14307130198 kanchan Linux ubuntu 3.19.0-56-generic #62~14.04.1-Ubuntu SMP Fri Mar 11 11:03:15 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  16:54:20 up  7:21,  2 users,  load average: 0.00, 0.07, 0.11 6231c3d87528447a638213cc63c6e156d7a28017

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -129,7 +129,7 @@ uint32_t loader() {
 
 			ramdisk_read((uint8_t*)(ph->p_vaddr), ph->p_offset, ph->p_filesz);
 			 
-			memset((void*)ph->p_vaddr + ph->p_filesz, ph->p_memsz - ph->p_filesz, 0);
+			memset((void*)ph->p_vaddr + ph->p_filesz, 0, ph->p_memsz - ph->p_filesz);
 
 
 #ifdef IA32_PAGE
