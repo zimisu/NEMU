a9ab0d61c82b331178f3c1d3cb900666ef600ac8
tracer-ics2015 <tracer@njuics.org>
2016-03-15 19:29:33 +0800

    >  compile kernel 14307130198 kanchan Linux ubuntu 3.19.0-56-generic #62~14.04.1-Ubuntu SMP Fri Mar 11 11:03:15 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  19:29:33 up  9:20,  2 users,  load average: 0.14, 0.11, 0.09 81873497ef0d6fa082bd76222ed4caf91b8702ad

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -80,7 +80,7 @@ uint32_t loader() {
 
 
 
-
+/*
 
 #include "common.h"
 #include "memory.h"
@@ -117,17 +117,15 @@ uint32_t loader() {
 	volatile uint32_t entry = elf->e_entry;
 	nemu_assert(entry != 0);
 	
-	/* TODO: fix the magic number with the correct one */
 	const uint32_t elf_magic = 0x464c457f;
 	uint32_t *p_magic = (void *)buf;
 	nemu_assert(*p_magic == elf_magic);
 
-	/* Load each program segment */
 	//panic("please implement me");
 	int i;
-	ph = ((void*)buf) + elf->e_phoff;	
+	ph = (void*)buf + elf->e_phoff;	
 	for(i = 0; i < elf->e_phnum; i++) {
-		/* Scan the program header table, load each segment into memory */
+		// Scan the program header table, load each segment into memory 
 		if(ph->p_type == PT_LOAD) {
 
 			ramdisk_read((uint8_t*)(ph->p_vaddr), ELF_OFFSET_IN_DISK + ph->p_offset, ph->p_filesz);
@@ -136,7 +134,7 @@ uint32_t loader() {
 
 
 #ifdef IA32_PAGE
-			/* Record the program break for future use. */
+			// Record the program break for future use. 
 			extern uint32_t brk;
 			uint32_t new_brk = ph->p_vaddr + ph->p_memsz - 1;
 			if(brk < new_brk) { brk = new_brk; }
@@ -159,7 +157,107 @@ uint32_t loader() {
 	return entry;
 	//return 0x800000;
 }
+*/
 
+
+#include "common.h"
+#include "memory.h"
+#include <string.h>
+#include <elf.h>
+//#include <stdio.h>
+
+#define ELF_OFFSET_IN_DISK 0
+
+#ifdef HAS_DEVICE
+void ide_read(uint8_t *, uint32_t, uint32_t);
+#else
+void ramdisk_read(uint8_t *, uint32_t, uint32_t);
+#endif
+
+#define HEAD_SIZE 4096
+#define STACK_SIZE (1 << 20)
+#define _SIZE_ (1 << 20)
+//0xa0000 - 1
+
+void create_video_mapping();
+uint32_t get_ucr3();
+
+uint32_t loader() {
+	Elf32_Ehdr *elf;
+	Elf32_Phdr *ph = NULL;
+
+	uint8_t buf[HEAD_SIZE];
+	uint8_t buf_[_SIZE_];
+
+#ifdef HAS_DEVICE
+	ide_read(buf, ELF_OFFSET_IN_DISK, HEAD_SIZE);
+#else
+	ramdisk_read(buf, ELF_OFFSET_IN_DISK, HEAD_SIZE);
+#endif
+	elf = (void*)buf;
+
+	/* TODO: fix the magic number with the correct one */
+	const uint32_t elf_magic = 0x464c457f;
+	uint32_t *p_magic = (void *)buf;
+	nemu_assert(*p_magic == elf_magic);
+
+	int cnt;
+	/* Load each program segment */
+	for(cnt = 0; cnt < elf->e_phnum; ++ cnt) {
+		/* Scan the program header table, load each segment into memory */
+/*
+#ifdef HAS_DEVICE
+		ide_read(buf_, ELF_OFFSET_IN_DISK + elf->e_ehsize + cnt * elf->e_phentsize, elf->e_phentsize);
+#else
+		ramdisk_read(buf_, ELF_OFFSET_IN_DISK + elf->e_ehsize + cnt * elf->e_phentsize, elf->e_phentsize);
+#endif
+*/
+		ph = (void*)(buf + elf->e_ehsize + cnt * elf->e_phentsize);
+//		ph = (void*)buf_;
+		if(ph->p_type == PT_LOAD) {
+			/* TODO: read the content of the segment from the ELF file 
+			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
+			 */
+#ifdef HAS_DEVICE
+		ide_read(buf_, ELF_OFFSET_IN_DISK + ph->p_offset, ph->p_filesz);
+#else
+		ramdisk_read(buf_, ELF_OFFSET_IN_DISK + ph->p_offset, ph->p_filesz);
+#endif		
+			
+			uint32_t hwaddr = 
+				mm_malloc(ph->p_vaddr, ph->p_memsz);
+
+			memcpy((void *)hwaddr, (void *)(buf_), ph->p_filesz);
+			/* TODO: zero the memory region 
+			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
+			 */
+			
+//			Log("%x %x %x %x", ph->p_vaddr, ph->p_filesz, ph->p_memsz, ph->p_vaddr + ph->p_filesz);
+			memset((void *)hwaddr + ph->p_filesz, 0, ph->p_memsz - ph->p_filesz);
+
+#ifdef IA32_PAGE
+			/* Record the program break for future use. */
+			extern uint32_t brk;
+			uint32_t new_brk = ph->p_vaddr + ph->p_memsz - 1;
+			if(brk < new_brk) { brk = new_brk; }
+#endif
+		}
+	}
+
+	volatile uint32_t entry = elf->e_entry;
+
+#ifdef IA32_PAGE
+	mm_malloc(KOFFSET - STACK_SIZE, STACK_SIZE);
+
+#ifdef HAS_DEVICE
+	create_video_mapping();
+#endif
+	write_cr3(get_ucr3());
+#endif
+
+//	HIT_GOOD_TRAP;
+	return entry;
+}
 
 
 
