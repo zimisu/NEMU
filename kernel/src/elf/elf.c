e2cf95b3114103aad979400feb46fa6eb9440063
tracer-ics2015 <tracer@njuics.org>
2016-03-16 14:17:46 +0800

    >  compile kernel 14307130198 kanchan Linux ubuntu 3.19.0-56-generic #62~14.04.1-Ubuntu SMP Fri Mar 11 11:03:15 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  14:17:46 up 20:56,  4 users,  load average: 0.14, 0.18, 0.15 5acf356ff8d983cf123ea515676718caa1c1753e

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -31,7 +31,7 @@ uint32_t loader() {
 #else
 	ramdisk_read(buf, ELF_OFFSET_IN_DISK, HEAD_SIZE);
 #endif
-	set_bp();
+	//set_bp();
 	elf = (void*)buf;
 
 	/* TODO: fix the magic number with the correct one */
@@ -42,20 +42,20 @@ uint32_t loader() {
 	int cnt;
 	/* Load each program segment */
 	ph = (void*)buf + elf->e_phoff;		
-	set_bp();	
+	//set_bp();	
 	int size = sizeof(Elf32_Phdr);
-	set_bp();
+	//set_bp();
 	size ++;				
 	for(cnt = 0; cnt < elf->e_phnum; ++ cnt) {
 		/* Scan the program header table, load each segment into memory */
 
 		if(ph->p_type == PT_LOAD) {
 			
-			set_bp();
+			//set_bp();
 			ramdisk_read((void*)ph->p_vaddr, ph->p_offset, ph->p_filesz);
 			//memcpy((void *)ph->p_vaddr, (void *)(buf) + ph->p_offset, ph->p_filesz);
 //			Log("%x %x %x %x", ph->p_vaddr, ph->p_filesz, ph->p_memsz, ph->p_vaddr + ph->p_filesz);
-			memset((void *)ph->p_vaddr + ph->p_filesz, 0, ph->p_memsz - ph->p_filesz);
+			memset((void *)ph->p_vaddr				 + ph->p_filesz, 0, ph->p_memsz - ph->p_filesz);
 
 #ifdef IA32_PAGE
 			/* Record the program break for future use. */
@@ -66,7 +66,7 @@ uint32_t loader() {
 		}
 		//set_bp();	
 		ph++;
-		set_bp();
+		//set_bp();
 	}
 
 	volatile uint32_t entry = elf->e_entry;
