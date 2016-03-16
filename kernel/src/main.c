894286ecb973e7ca138d5a244f29917b29ae314d
tracer-ics2015 <tracer@njuics.org>
2016-03-16 11:07:39 +0800

    >  compile kernel 14307130198 kanchan Linux ubuntu 3.19.0-56-generic #62~14.04.1-Ubuntu SMP Fri Mar 11 11:03:15 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  11:07:39 up 19:26,  4 users,  load average: 0.14, 0.09, 0.06 a669e909863277d959381d79a6100d3a90d92b82

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -99,7 +99,7 @@ void init_cond() {
 
 	/* Keep the `bt' command happy. */
 	//asm volatile("movl $0, %ebp");
-	asm volatile("subl $16, %esp");
+	//asm volatile("subl $16, %esp");
 
 	/* Here we go! */
 	((void(*)(void))eip)();
