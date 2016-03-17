44c879518f6ac60b8540f717511c71a47a9352a3
tracer-ics2015 <tracer@njuics.org>
2016-03-17 15:18:54 +0800

    >  run 14307130198 kanchan Linux ubuntu 3.19.0-56-generic #62~14.04.1-Ubuntu SMP Fri Mar 11 11:03:15 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  15:18:54 up 16:59,  6 users,  load average: 0.13, 0.13, 0.14 b8c622c0c51fb956357176d05f610688d949383

--- testcase/src/hello-str.c
+++ testcase/src/hello-str.c
@@ -18,16 +18,16 @@ int main() {
 	sprintf(buf, "%s", "Hello world!\n");
 //set_bp();
 	int res = strcmp("1", "1");
-//set_bp();
+set_bp();
 	nemu_assert(res == 0);
-//set_bp();
+set_bp();
 	sprintf(buf, "%d + %d = %d\n", 1, 1, 2);
 	nemu_assert(strcmp(buf, "1 + 1 = 2\n") == 0);
-//set_bp();
+set_bp();
 
 	sprintf(buf, "%d + %d = %d\n", 2, 10, 12);
 	nemu_assert(strcmp(buf, "2 + 10 = 12\n") == 0);
-//set_bp();
+set_bp();
 
 	HIT_GOOD_TRAP;
 	return 0;
