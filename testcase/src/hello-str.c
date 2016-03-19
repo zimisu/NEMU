41a6d21c3f2a20e5d89a5344e27bdceb3430016a
tracer-ics2015 <tracer@njuics.org>
2016-03-19 19:40:19 +0800

    >  run 14307130198 kanchan Linux ubuntu 3.19.0-56-generic #62~14.04.1-Ubuntu SMP Fri Mar 11 11:03:15 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  19:40:19 up 17:51,  5 users,  load average: 0.23, 0.14, 0.14 827d424081b24e8b65769d134b5ac0b7cb70318a

--- testcase/src/hello-str.c
+++ testcase/src/hello-str.c
@@ -15,8 +15,9 @@ void *sbrk(int incr) {
 
 int main() {
 	sprintf(buf, "%s", "Hello world!\n");
+	set_bp();
 	nemu_assert(strcmp(buf, "Hello world!\n") == 0);
-
+	set_bp();
 	sprintf(buf, "%d + %d = %d\n", 1, 1, 2);
 	nemu_assert(strcmp(buf, "1 + 1 = 2\n") == 0);
 
