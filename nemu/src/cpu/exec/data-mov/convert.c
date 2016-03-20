3e876c2c8ff9f8cc14ffe078a49a7e3a4ddda192
tracer-ics2015 <tracer@njuics.org>
2016-03-20 16:12:25 +0800

    >  compile NEMU 14307130198 kanchan Linux ubuntu 3.19.0-56-generic #62~14.04.1-Ubuntu SMP Fri Mar 11 11:03:15 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  16:12:25 up  2:03,  2 users,  load average: 0.05, 0.06, 0.09 8ed40990f2d42d98f5496b73d83cdd8af847345f

--- nemu/src/cpu/exec/data-mov/convert.c
+++ nemu/src/cpu/exec/data-mov/convert.c
@@ -2,6 +2,9 @@
 
 make_helper(cvt)
 {
+	if (cpu.eax >> 31) cpu.edx = 0xffffffff;
+		else cpu.edx = 0;
+/*
     if (ops_decoded.is_data_size_16)
     {
         if (cpu.ax & 0x8000) cpu.dx = 0xffff;
@@ -13,7 +16,7 @@ make_helper(cvt)
         if (cpu.eax & 0x80000000)
             cpu.edx = 0xffffffff;
 		else cpu.edx = 0;
-    }
+    }*/
     
     print_asm("cwd/cdq");
     return 1;
