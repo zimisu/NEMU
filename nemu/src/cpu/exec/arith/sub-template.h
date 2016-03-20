dd4b02e209a24a5aacf6d86893d02547f537ef20
tracer-ics2015 <tracer@njuics.org>
2016-03-20 17:56:50 +0800

    >  compile NEMU 14307130198 kanchan Linux ubuntu 3.19.0-56-generic #62~14.04.1-Ubuntu SMP Fri Mar 11 11:03:15 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  17:56:50 up  3:27,  3 users,  load average: 0.09, 0.07, 0.11 89e91910edf473877ced9c1c36397aa827b23b0e

--- nemu/src/cpu/exec/arith/sub-template.h
+++ nemu/src/cpu/exec/arith/sub-template.h
@@ -32,7 +32,6 @@ static void do_execute() {
 	cpu.EFLAGS.CF = a < b;
 	cpu.EFLAGS.ZF = (ans == 0);
 	cpu.EFLAGS.OF = MSB(a) != MSB(b) && MSB(a) != MSB(ans);
-	//cpu.EFLAGS.OF = (((a ^ b) & ans & b) >> (bits - 1)) & 1;
 	cpu.EFLAGS.SF = MSB(ans);
 
 	cpu.EFLAGS.PF = get_pf(ans);
--- nemu/src/cpu/exec/call/call-template.h
+++ nemu/src/cpu/exec/call/call-template.h
@@ -12,8 +12,8 @@
 static void do_execute()
 {
 	cpu.esp -= 4;
-	swaddr_write(cpu.esp, 4, cpu.eip + get_instr_len());
-	//swaddr_write(cpu.esp, 4, cpu.eip + DATA_BYTE + 1);
+	//swaddr_write(cpu.esp, 4, cpu.eip + get_instr_len());
+	swaddr_write(cpu.esp, 4, cpu.eip + DATA_BYTE + 1);
 
 	if (op_src->type == OP_TYPE_IMM)
 		cpu.eip += op_src->val;
