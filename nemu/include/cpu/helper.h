9b0e970e43b2e0f1bb65500ad9e11447357831ba
tracer-ics2015 <tracer@njuics.org>
2016-03-22 13:54:09 +0800

    >  compile NEMU 14307130198 kanchan Linux ubuntu 3.19.0-56-generic #62~14.04.1-Ubuntu SMP Fri Mar 11 11:03:15 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  13:54:09 up  9:55,  2 users,  load average: 0.00, 0.02, 0.05 db8da9c3dc4c71855ca3d19b9b6c02bdb6102630

--- nemu/include/cpu/helper.h
+++ nemu/include/cpu/helper.h
@@ -10,7 +10,6 @@
 static inline uint32_t instr_fetch(swaddr_t addr, size_t len) {
 	return swaddr_read(addr, len);
 }
-static inline int alksjgdlkasd(){return 1;}
 
 static int len;
 /* Instruction Decode and EXecute */
