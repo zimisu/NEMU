990f564bbe5ea2171a6ad4bc6d37e196a8d951bc
tracer-ics2015 <tracer@njuics.org>
2016-04-29 00:29:26 +0800

    >  compile NEMU 14307130198 kanchan Linux ubuntu 3.19.0-56-generic #62~14.04.1-Ubuntu SMP Fri Mar 11 11:03:15 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  00:29:26 up 1 day,  8:58,  2 users,  load average: 0.16, 0.16, 0.14 276743bfd6b6acc4f5b33fe7f009a4dc118a822

--- nemu/src/memory/sreg.c
+++ nemu/src/memory/sreg.c
@@ -63,7 +63,8 @@ void load_sreg(uint32_t sreg) {
 	printf("sreg:%d\n", sreg);
 	for (i = 0; i < 8; i++)
 		printf("%x\n", tmp[i]);
-	Assert(segdesc->present == 1, "Segdesc is not valid! 0x%x  segdesc::0x%x %x", cpu.GDTR.base + cpu.sr[sreg].index, *(int*)segdesc, *((int*)segdesc) + 1);
+	Assert(segdesc->present == 1, "Segdesc is not valid! 0x%x  segdesc::0x%x %x", 
+		cpu.GDTR.base + cpu.sr[sreg].index * 8, *(int*)segdesc, *((int*)segdesc) + 1);
 	Assert(cpu.sr[sreg].index * 8 < (segdesc->limit_19_16 << 16) + segdesc->limit_15_0, "Segment overflow!");
 	cpu.sr[sreg].invi = *segdesc;
 }
