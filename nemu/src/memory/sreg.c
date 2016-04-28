cdb377925b4d00f0462b53f4374ca16c5eed20d2
tracer-ics2015 <tracer@njuics.org>
2016-04-28 16:47:44 +0800

    >  compile NEMU 14307130198 kanchan Linux ubuntu 3.19.0-56-generic #62~14.04.1-Ubuntu SMP Fri Mar 11 11:03:15 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  16:47:44 up 1 day,  2:18,  2 users,  load average: 0.28, 0.18, 0.14 36be97d33fa4480ae1fad667e7fa8fff6f9a8293

--- nemu/src/memory/sreg.c
+++ nemu/src/memory/sreg.c
@@ -15,6 +15,7 @@ void init_seg() {
 	segdesc->limit_19_16 = segdesc->limit_15_0 = -1;
 	cpu.sr[R_SS].invi = *segdesc;
 	cpu.cr._[0] = cpu.cr._[3] = 0;
+	printf("%x  %x\n", *(int*)segdesc, *((int*)segdesc) + 1);
 }
 
 lnaddr_t seg_translate(swaddr_t addr, uint8_t sreg) {
