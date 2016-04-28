9eb99b9546b7c3b4d0b0f080250f89237c30b45e
tracer-ics2015 <tracer@njuics.org>
2016-04-28 23:25:30 +0800

    >  compile NEMU 14307130198 kanchan Linux ubuntu 3.19.0-56-generic #62~14.04.1-Ubuntu SMP Fri Mar 11 11:03:15 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  23:25:30 up 1 day,  7:54,  2 users,  load average: 0.16, 0.15, 0.14 1c6526aa978308b4a22414b82e3afb439be67a

--- nemu/src/memory/sreg.c
+++ nemu/src/memory/sreg.c
@@ -6,12 +6,12 @@ uint32_t lnaddr_read(hwaddr_t, size_t);
 uint32_t hwaddr_read(hwaddr_t, size_t);
 uint32_t hwaddr_write(hwaddr_t, size_t, uint32_t);
 
-void init_seg() {
+void init_seg() {	
 	uint8_t tmp[8];	
 	cpu.cs = 8;
 	SegDesc *segdesc = (SegDesc*)tmp;
-	segdesc->base_31_24 = segdesc->base_23_16 = segdesc->base_15_0 = 0;
 	segdesc->present = 1;
+	segdesc->base_31_24 = segdesc->base_23_16 = segdesc->base_15_0 = 0;
 	segdesc->limit_19_16 = segdesc->limit_15_0 = -1;
 	cpu.sr[R_SS].invi = *segdesc;
 	cpu.cr._[0] = cpu.cr._[3] = 0;
@@ -34,7 +34,7 @@ void load_sreg(uint32_t sreg) {
 	uint8_t tmp[8]; 
 	int i;
 	for(i = 0; i < 8; ++ i) 
-		tmp[i] = lnaddr_read(cpu.GDTR.base + cpu.sr[sreg].index * 8 + i, 1);
+		tmp[i] = lnaddr_read(cpu.GDTR.base + cpu.sr[sreg].index + i, 1);
 	SegDesc *segdesc = (SegDesc*)tmp;
 
 	for (i = 0; i < 8; i++)
