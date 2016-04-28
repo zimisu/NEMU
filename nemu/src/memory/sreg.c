3c9cfb13ceb0182848efa06be3aeb7109393bbea
tracer-ics2015 <tracer@njuics.org>
2016-04-29 00:26:46 +0800

    >  compile NEMU 14307130198 kanchan Linux ubuntu 3.19.0-56-generic #62~14.04.1-Ubuntu SMP Fri Mar 11 11:03:15 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  00:26:46 up 1 day,  8:55,  2 users,  load average: 0.13, 0.14, 0.14 dd9d9f32d3dfb6289add7bb4b5287dc411827229

--- nemu/src/memory/sreg.c
+++ nemu/src/memory/sreg.c
@@ -9,6 +9,7 @@ uint32_t hwaddr_write(hwaddr_t, size_t, uint32_t);
 void init_seg() {	
 	uint8_t tmp[8];	
 	cpu.cs = 8;
+	/*
 	printf("%d %d %d %d----\n", cpu.sr[0]._16, cpu.sr[1]._16, cpu.sr[2]._16, cpu.sr[3]._16);
 	printf("%llu %llu %llu %llu\n", 
 		(unsigned long long)cpu.sr[0].value, 
@@ -16,11 +17,13 @@ void init_seg() {
 		(unsigned long long)cpu.sr[2].value, 
 		(unsigned long long)cpu.sr[3].value);
 	printf("R_CS:%d\n  cs:%d  es index:%d\n", R_CS, cpu.cs, cpu.sr[R_CS].index);
+	*/
 	SegDesc *segdesc = (SegDesc*)tmp;
 	segdesc->present = 1;
 	segdesc->base_31_24 = segdesc->base_23_16 = segdesc->base_15_0 = 0;
 	segdesc->limit_19_16 = segdesc->limit_15_0 = -1;
 	cpu.sr[R_CS].invi = *segdesc;
+	/*
 	printf("R_CS:%d\n  _16:%d  %d %d es index:%d\n", R_CS, cpu.sr[R_CS]._16, 
 		cpu.sr[R_CS].rpl, 
 		cpu.sr[R_CS].ti, 
@@ -33,6 +36,7 @@ void init_seg() {
 		(unsigned long long)cpu.sr[1].value, 
 		(unsigned long long)cpu.sr[2].value, 
 		(unsigned long long)cpu.sr[3].value);
+		*/
 	cpu.cr._[0] = cpu.cr._[3] = 0;
 	int i;
 	for (i = 0; i < 8; i++)
