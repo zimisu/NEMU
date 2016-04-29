5cffdfc5f612236852bc2e57a023c36616d0bd1d
tracer-ics2015 <tracer@njuics.org>
2016-04-29 20:25:25 +0800

    >  compile kernel 14307130198 kanchan Linux ubuntu 3.19.0-56-generic #62~14.04.1-Ubuntu SMP Fri Mar 11 11:03:15 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  20:25:25 up 1 day, 17:16,  3 users,  load average: 0.09, 0.13, 0.16 9d1586945f363dc5a6edbe2ff3ed1d3dd213507

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -12,7 +12,7 @@ void ide_read(uint8_t *, uint32_t, uint32_t);
 void ramdisk_read(uint8_t *, uint32_t, uint32_t);
 #endif
 
-#define HEAD_SIZE 409600
+#define HEAD_SIZE 4096
 #define STACK_SIZE (1 << 20)
 #define _SIZE_ (1 << 20)
 //0xa0000 - 1
@@ -25,13 +25,13 @@ uint32_t loader() {
 	Elf32_Phdr *ph = NULL;
 
 	uint8_t buf[HEAD_SIZE];
+	uint8_t buf_[_SIZE_];
 
 #ifdef HAS_DEVICE
 	ide_read(buf, ELF_OFFSET_IN_DISK, HEAD_SIZE);
 #else
 	ramdisk_read(buf, ELF_OFFSET_IN_DISK, HEAD_SIZE);
 #endif
-	//set_bp();
 	elf = (void*)buf;
 
 	/* TODO: fix the magic number with the correct one */
@@ -41,32 +41,45 @@ uint32_t loader() {
 
 	int cnt;
 	/* Load each program segment */
-	ph = (void*)buf + elf->e_phoff;		
-	//set_bp();	
-	int size = sizeof(Elf32_Phdr);
-	//set_bp();
-	size ++;				
 	for(cnt = 0; cnt < elf->e_phnum; ++ cnt) {
 		/* Scan the program header table, load each segment into memory */
-
+/*
+#ifdef HAS_DEVICE
+		ide_read(buf_, ELF_OFFSET_IN_DISK + elf->e_ehsize + cnt * elf->e_phentsize, elf->e_phentsize);
+#else
+		ramdisk_read(buf_, ELF_OFFSET_IN_DISK + elf->e_ehsize + cnt * elf->e_phentsize, elf->e_phentsize);
+#endif
+*/
+		ph = (void*)(buf + elf->e_ehsize + cnt * elf->e_phentsize);
+//		ph = (void*)buf_;
 		if(ph->p_type == PT_LOAD) {
+			/* TODO: read the content of the segment from the ELF file 
+			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
+			 */
+#ifdef HAS_DEVICE
+		ide_read(buf_, ELF_OFFSET_IN_DISK + ph->p_offset, ph->p_filesz);
+#else
+		ramdisk_read(buf_, ELF_OFFSET_IN_DISK + ph->p_offset, ph->p_filesz);
+#endif		
 			
-			//set_bp();
-			ramdisk_read((uint8_t*)ph->p_vaddr, ph->p_offset, ph->p_filesz);
-			//memcpy((void *)ph->p_vaddr, (void *)(buf) + ph->p_offset, ph->p_filesz);
+			uint32_t hwaddr = 
+				mm_malloc(ph->p_vaddr, ph->p_memsz);
+
+			memcpy((void *)hwaddr, (void *)(buf_), ph->p_filesz);
+			/* TODO: zero the memory region 
+			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
+			 */
+			
 //			Log("%x %x %x %x", ph->p_vaddr, ph->p_filesz, ph->p_memsz, ph->p_vaddr + ph->p_filesz);
-			memset((void *)ph->p_vaddr + ph->p_filesz, 0, ph->p_memsz - ph->p_filesz);
+			memset((void *)hwaddr + ph->p_filesz, 0, ph->p_memsz - ph->p_filesz);
 
 #ifdef IA32_PAGE
 			/* Record the program break for future use. */
 			extern uint32_t brk;
 			uint32_t new_brk = ph->p_vaddr + ph->p_memsz - 1;
 			if(brk < new_brk) { brk = new_brk; }
-#endif	
+#endif
 		}
-		//set_bp();	
-		ph++;
-		//set_bp();
 	}
 
 	volatile uint32_t entry = elf->e_entry;
