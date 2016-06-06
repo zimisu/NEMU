85758e72cf943a8ea9c6e2e2e3d4a915d38c1081
tracer-ics2015 <tracer@njuics.org>
2016-06-06 19:24:38 +0800

    >  compile kernel 14307130198 kanchan Linux ubuntu 3.19.0-56-generic #62~14.04.1-Ubuntu SMP Fri Mar 11 11:03:15 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  19:24:38 up  7:57,  3 users,  load average: 0.17, 0.17, 0.15 48b9e3e7b4abfbddd01c0e49a3af8757b94206bc

--- kernel/src/syscall/do_syscall.c
+++ kernel/src/syscall/do_syscall.c
@@ -13,13 +13,15 @@ static void sys_brk(TrapFrame *tf) {
 }
 
 static void sys_write(TrapFrame *tf) {
+	/*
 #ifdef HAS_DEVICE
 	int i;
 	for(i = 0; i < tf->edx; ++ i)
 		serial_printc(*(char *)(tf->ecx + i));
-#else				
+#else
+*/				
 	asm volatile (".byte 0xd6" : : "a"(2), "c"(tf->ecx), "d"(tf->edx));
-#endif
+//#endif
 	tf->eax = tf->edx;
 }
 
