60f293661c888a1d6e2abd8e2c14b2b28cd901b5
tracer-ics2015 <tracer@njuics.org>
2016-06-06 23:13:34 +0800

    >  compile NEMU 14307130198 kanchan Linux ubuntu 3.19.0-56-generic #62~14.04.1-Ubuntu SMP Fri Mar 11 11:03:15 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  23:13:34 up 10:39,  3 users,  load average: 0.13, 0.14, 0.14 e2366c69ae545659b406eae7780379852388b89a

--- nemu/src/cpu/exec/data-mov/iret-template.h
+++ nemu/src/cpu/exec/data-mov/iret-template.h
@@ -13,7 +13,8 @@ static void do_execute() {
 	load_sreg(R_CS);
 
 	cpu.EFLAGS.all_EFLAGS = swaddr_read(cpu.esp, 4, R_SS);
-	cpu.esp += 3;
+	cpu.esp += 4;
+	cpu.eip -= 1;
 	printf("in iret: eip=%x\n", cpu.eip);
 	print_asm("iret");
 }
