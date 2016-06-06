3d81c216c6851d29f67de66b664d052a903deea5
Chen Kan <zimisu@gmail.com>
2016-06-06 17:50:27 +0800

    fix bugs.

--- kernel/src/syscall/do_syscall.c
+++ kernel/src/syscall/do_syscall.c
@@ -19,6 +19,7 @@ void do_syscall(TrapFrame *tf) {
 		 * very dangerous in a real operating system. Therefore such a 
 		 * system call never exists in GNU/Linux.
 		 */
+		 
 		case 0: 
 			cli();
 			add_irq_handle(tf->ebx, (void*)tf->ecx);
