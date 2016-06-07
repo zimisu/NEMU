5721d66b2ce3e08d0142d6adb1a1bbb5ca8bd5c3
tracer-ics2015 <tracer@njuics.org>
2016-06-07 19:22:04 +0800

    >  compile NEMU 14307130198 kanchan Linux ubuntu 3.19.0-56-generic #62~14.04.1-Ubuntu SMP Fri Mar 11 11:03:15 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  19:22:04 up 21:24,  3 users,  load average: 0.30, 0.28, 0.25 e6171abd97c08b445df2572c77eb2114142a5dc7

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -77,6 +77,7 @@ void init_cond() {
 	/* Write some test data to the video memory. */
 	Log("Hello, this is video_mapping_write_test!");
 	video_mapping_write_test();
+	Log("Hello, this is video_mapping_write_test finished!");
 #endif
 
 	Log("Hello, NEMU world before loader in kernel main.c!");
