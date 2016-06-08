826abc1a0578ac8d299c34e90f1ecf267d95bea0
tracer-ics2015 <tracer@njuics.org>
2016-06-08 10:42:30 +0800

    >  compile NEMU 14307130198 kanchan Linux ubuntu 3.19.0-56-generic #62~14.04.1-Ubuntu SMP Fri Mar 11 11:03:15 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  10:42:30 up 13:35,  2 users,  load average: 0.31, 0.23, 0.17 bcae2236172ea386100c24e16d076b620f965fb

--- nemu/src/device/sdl.c
+++ nemu/src/device/sdl.c
@@ -22,83 +22,90 @@ extern void timer_intr();
 extern void keyboard_intr();
 extern void update_screen();
 
+void device_update();
+
 static void timer_sig_handler(int signum) {
-    jiffy ++;
-    timer_intr();
+	printf("hit timer_sig_handler~~~~~~~~~~~\n");
+	jiffy ++;
+	timer_intr();
 
-    device_update_flag = true;
-    if(jiffy % (TIMER_HZ / VGA_HZ) == 0) {
-        update_screen_flag = true;
-    }
+	device_update_flag = true;
+	if(jiffy % (TIMER_HZ / VGA_HZ) == 0) {
+		printf("update_screen_flag=true\n");
+		update_screen_flag = true;
+		device_update();
+	}
 
-    int ret = setitimer(ITIMER_VIRTUAL, &it, NULL);
-    Assert(ret == 0, "Can not set timer");
+	int ret = setitimer(ITIMER_VIRTUAL, &it, NULL);
+	Assert(ret == 0, "Can not set timer");
 }
 
 void device_update() {
-    if(!device_update_flag) {
-        return;
-    }
-    device_update_flag = false;
+	printf("device_update hit!!!!!!!!!!!!!!");
+	if(!device_update_flag) {
+		return;
+	}
+	device_update_flag = false;
 
-    if(update_screen_flag) {
-        update_screen();
-        update_screen_flag = false;
-    }
+	if(update_screen_flag) {
+		update_screen();
+		update_screen_flag = false;
+	}
 
-    SDL_Event event;
-    while(SDL_PollEvent(&event)) {
-        // If a key was pressed
+	SDL_Event event;
+	while(SDL_PollEvent(&event)) {
+		// If a key was pressed
 
-        uint32_t sym = event.key.keysym.sym;
-        if( event.type == SDL_KEYDOWN ) {
-            keyboard_intr(sym2scancode[sym >> 8][sym & 0xff]);
-        }
-        else if( event.type == SDL_KEYUP ) {
-            keyboard_intr(sym2scancode[sym >> 8][sym & 0xff] | 0x80);
-        }
+		uint32_t sym = event.key.keysym.sym;
+		if( event.type == SDL_KEYDOWN ) {
+			keyboard_intr(sym2scancode[sym >> 8][sym & 0xff]);
+		}
+		else if( event.type == SDL_KEYUP ) {
+			keyboard_intr(sym2scancode[sym >> 8][sym & 0xff] | 0x80);
+		}
 
-        // If the user has Xed out the window
-        if( event.type == SDL_QUIT ) {
-            //Quit the program
-            exit(0);
-        }
-    }
+		// If the user has Xed out the window
+		if( event.type == SDL_QUIT ) {
+			//Quit the program
+			exit(0);
+		}
+	}
 }
 
 void sdl_clear_event_queue() {
-    SDL_Event event;
-    while(SDL_PollEvent(&event));
+	SDL_Event event;
+	while(SDL_PollEvent(&event));
 }
 
 void init_sdl() {
-    int ret = SDL_Init(SDL_INIT_VIDEO | SDL_INIT_NOPARACHUTE);
-    Assert(ret == 0, "SDL_Init failed");
+	printf("init_sdl===========================\n");
+	int ret = SDL_Init(SDL_INIT_VIDEO | SDL_INIT_NOPARACHUTE);
+	Assert(ret == 0, "SDL_Init failed");
 
-    real_screen = SDL_SetVideoMode(640, 400, 8, 
-            SDL_HWSURFACE | SDL_HWPALETTE | SDL_HWACCEL | SDL_ASYNCBLIT);
+	real_screen = SDL_SetVideoMode(640, 400, 8, 
+			SDL_HWSURFACE | SDL_HWPALETTE | SDL_HWACCEL | SDL_ASYNCBLIT);
 
-    screen = SDL_CreateRGBSurface(SDL_SWSURFACE, 640, 400, 8,
-            real_screen->format->Rmask, real_screen->format->Gmask,
-            real_screen->format->Bmask, real_screen->format->Amask);
-    pixel_buf = screen->pixels;
+	screen = SDL_CreateRGBSurface(SDL_SWSURFACE, 640, 400, 8,
+			real_screen->format->Rmask, real_screen->format->Gmask,
+			real_screen->format->Bmask, real_screen->format->Amask);
+	pixel_buf = screen->pixels;
 
-    SDL_SetPalette(real_screen, SDL_LOGPAL | SDL_PHYSPAL, (void *)&palette, 0, 256);
-    SDL_SetPalette(screen, SDL_LOGPAL, (void *)&palette, 0, 256);
+	SDL_SetPalette(real_screen, SDL_LOGPAL | SDL_PHYSPAL, (void *)&palette, 0, 256);
+	SDL_SetPalette(screen, SDL_LOGPAL, (void *)&palette, 0, 256);
 
-    SDL_WM_SetCaption("NEMU", NULL);
+	SDL_WM_SetCaption("NEMU", NULL);
 
-    SDL_EnableKeyRepeat(SDL_DEFAULT_REPEAT_DELAY, SDL_DEFAULT_REPEAT_INTERVAL);
+	SDL_EnableKeyRepeat(SDL_DEFAULT_REPEAT_DELAY, SDL_DEFAULT_REPEAT_INTERVAL);
 
-    struct sigaction s;
-    memset(&s, 0, sizeof(s));
-    s.sa_handler = timer_sig_handler;
-    ret = sigaction(SIGVTALRM, &s, NULL);
-    Assert(ret == 0, "Can not set signal handler");
+	struct sigaction s;
+	memset(&s, 0, sizeof(s));
+	s.sa_handler = timer_sig_handler;
+	ret = sigaction(SIGVTALRM, &s, NULL);
+	Assert(ret == 0, "Can not set signal handler");
 
-    it.it_value.tv_sec = 0;
-    it.it_value.tv_usec = 1000000 / TIMER_HZ;
-    ret = setitimer(ITIMER_VIRTUAL, &it, NULL);
-    Assert(ret == 0, "Can not set timer");
+	it.it_value.tv_sec = 0;
+	it.it_value.tv_usec = 1000000 / TIMER_HZ;
+	ret = setitimer(ITIMER_VIRTUAL, &it, NULL);
+	Assert(ret == 0, "Can not set timer");
 }
-#endif    /* HAS_DEVICE */
+#endif	/* HAS_DEVICE */
