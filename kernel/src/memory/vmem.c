b66f1437883d33ada18707046e24bef7d28344c3
tracer-ics2015 <tracer@njuics.org>
2016-06-13 17:22:47 +0800

    >  compile kernel 14307130198 kanchan Linux ubuntu 3.19.0-56-generic #62~14.04.1-Ubuntu SMP Fri Mar 11 11:03:15 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  17:22:47 up  2:06,  3 users,  load average: 0.17, 0.25, 0.34 a03abe23963fbaff7de8b4f42002752b8c0d9458

--- kernel/src/memory/vmem.c
+++ kernel/src/memory/vmem.c
@@ -5,33 +5,37 @@
 #define VMEM_ADDR 0xa0000
 #define SCR_SIZE (320 * 200)
 
+static PTE vptable[/*(VMEM_ADDR + SCR_SIZE) / PAGE_SIZE + 1*/NR_PTE] align_to_page;
+
 /* Use the function to get the start address of user page directory. */
 inline PDE* get_updir();
-static PTE vptable[NR_PTE] align_to_page;
+
 void create_video_mapping() {
 	/* TODO: create an identical mapping from virtual memory area 
 	 * [0xa0000, 0xa0000 + SCR_SIZE) to physical memory area 
 	 * [0xa0000, 0xa0000 + SCR_SIZE) for user program. You may define
 	 * some page tables to create this mapping.
 	 */
-
-	//Log("hit create_video_mapping");
-	PDE *updir = get_updir();
-    PTE *ptable = (PTE *)va_to_pa(vptable);
-    updir[0].val = make_pde(ptable);
-
-    memset(vptable, 0, sizeof(vptable));
-
-    uint32_t vmem_addr = VMEM_ADDR;
-    uint32_t idx = VMEM_ADDR / PAGE_SIZE;
-    for (vmem_addr = VMEM_ADDR; vmem_addr < VMEM_ADDR + SCR_SIZE + PAGE_SIZE; vmem_addr += PAGE_SIZE) {
-        vptable[idx].val = make_pte(vmem_addr);
-        idx++;
-    }
+//	panic("please implement me");
+	PDE *pdir = get_updir();
+	PTE *ptable = /*(PTE *)va_to_pa*/(vptable + (VMEM_ADDR >> 12));
+/*	int pdir_idx;
+	for(pdir_idx = 0; pdir_idx < SCR_SIZE / PT_SIZE; pdir_idx ++) {
+		pdir[pdir_idx].val = make_pde(ptable);
+	}
+*/
+	pdir[0].val = make_pde(va_to_pa(ptable));
+	int pframe_addr;
+//	for(pframe_addr = 0; pframe_addr < VMEM_ADDR; pframe_addr += PAGE_SIZE) ptable ++;
+	for(pframe_addr = VMEM_ADDR; pframe_addr < VMEM_ADDR + SCR_SIZE + PAGE_SIZE; pframe_addr += PAGE_SIZE) {
+		ptable->val = make_pte(pframe_addr);
+		ptable ++;
+	}
 }
+
 void video_mapping_write_test() {
 	int i;
-	uint32_t *buf = (void *)VMEM_ADDR;
+	uint32_t *buf = (void *)(VMEM_ADDR);
 	for(i = 0; i < SCR_SIZE / 4; i ++) {
 		buf[i] = i;
 	}
@@ -40,7 +44,9 @@ void video_mapping_write_test() {
 void video_mapping_read_test() {
 	int i;
 	uint32_t *buf = (void *)VMEM_ADDR;
+//		assert(buf[SCR_SIZE / 4 - 1] == SCR_SIZE / 4 - 1);
 	for(i = 0; i < SCR_SIZE / 4; i ++) {
+//		if( buf[i] != i ) Log("%d %d", i, buf[i]);	
 		assert(buf[i] == i);
 	}
 }
